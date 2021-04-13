92b9ae702b27640c31765a525f482843ba251495
Aris Nur <ariezncahyo@gmail.com>
2021-04-14 05:07:45 +0700

    Setup struktur project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import logo from '../logo.svg';
+import '../styles/App.css';
+import {Button} from '@material-ui/core';
+function App() {
+  return (
+    <div className="App">
+      <Button>React Redux Kalkulator</Button>
+    </div>
+  );
+}
+
+export default App;
